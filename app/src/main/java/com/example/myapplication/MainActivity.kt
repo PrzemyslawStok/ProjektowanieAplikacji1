d7120e3688d59bc085af7a3fb78e39fe32f2e3a4
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-10-13 19:24:07 +0200

    cd ..

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +47,11 @@ class MainActivity : ComponentActivity() {
             }
             Image(
                 painter = painterResource(id = R.mipmap.ic_image),
-                contentDescription = "Simple image"
+                contentDescription = "Simple image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .alpha(0.1f)
+                //.clip(CircleShape)
             )
         }
     }
