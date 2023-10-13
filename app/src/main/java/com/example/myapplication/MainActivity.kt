259ac7d5656f1320be77585ab732b8f16323a15a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2023-10-13 19:17:02 +0200

    cd ..

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -16,12 +20,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Hello("Przemysław")
+            Add(10.0, 5.0)
         }
     }
 
     @Composable
-    fun Hello(name: String){
+    fun Hello(name: String) {
         Text(text = "Witaj $name!")
+    }
+
+    @Composable
+    fun Add(a: Double, b: Double) {
+        Row {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = "Simple image"
+            )
+            Column {
+                Text(text = "a=$a")
+                Text(text = "b=$b")
+            }
+            Image(
+                painter = painterResource(id = R.mipmap.ic_image),
+                contentDescription = "Simple image"
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewScreen() {
+        Add(2.0, 1.0)
     }
 }
